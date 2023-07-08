cec1a82617fbefaf0e67a08870811644542717fd
austinthomas147 <austinthomas147@gmail.com>
2023-07-08 14:17:36 -0500

    Add message count repository for tracking total messages received in the server lifespan

---
+++ PrimeNumberService.Common/Implementations/MessageCountRepository.cs
@@ -0,0 +1,27 @@
+﻿using PrimeNumberService.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberService.Common.Implementations
+{
+    public class MessageCountRepository : IMessageCountRepository
+    {
+        private int _messageCount;
+
+        public MessageCountRepository()
+        {
+            _messageCount = 0;
+        }
+
+        public Task AddMessage()
+        {
+            _messageCount++;
+            return Task.CompletedTask;
+        }
+
+        public Task<int> GetMessageCountAsync() => Task.FromResult(_messageCount);
+    }
+}
