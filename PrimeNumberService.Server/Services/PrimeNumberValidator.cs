69bafb2cc64094b670c8422559d277c71e0753c0
austinthomas147 <austinthomas147@gmail.com>
2023-07-07 22:17:35 -0500

    Add the PrimeNumberValidator service to receive messages via grpc and build a response back to the client

---
+++ PrimeNumberService.Server/Services/PrimeNumberValidator.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using PrimeNumberService.Server.Protos;
+using PrimeNumberService.Server.Utilities;
+
+namespace PrimeNumberService.Server.Services
+{
+    public class PrimeNumberValidator : Protos.PrimeNumberValidator.PrimeNumberValidatorBase
+    {
+        private readonly ILogger<PrimeNumberValidator> _logger;
+
+        public PrimeNumberValidator(ILogger<PrimeNumberValidator> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<PrimeNumberResponse> CheckPrimeNumber(PrimeNumberRequest request, 
+                                                                   ServerCallContext context)
+        {
+            var response = BuildPrimeNumberResponse(request);
+
+            return Task.FromResult(response);
+        }
+
+        private PrimeNumberResponse BuildPrimeNumberResponse(PrimeNumberRequest request)
+        {
+            var response = new PrimeNumberResponse();
+
+            response.RequestId = request.Id;
+            response.IsPrimeNumber = PrimeNumberHelper.IsPrimeNumber(request.Number);
+            response.Number = request.Number;
+            response.IncomingTimestamp = request.Timestamp;
+
+            return response;
+        }
+    }
+}
