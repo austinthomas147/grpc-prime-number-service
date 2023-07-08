f48efe6b55d97f63a8723d11e24c87fd22b8a2dd
austinthomas147 <austinthomas147@gmail.com>
2023-07-08 14:17:56 -0500

    Add PrimeNumberRepository for tracking prime numbers passed in to the server

---
+++ PrimeNumberService.Common/Implementations/PrimeNumberRepository.cs
@@ -0,0 +1,53 @@
+﻿using PrimeNumberService.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberService.Common.Implementations
+{
+    public class PrimeNumberRepository : IPrimeNumberRepository
+    {
+        private Dictionary<long, int> _primeNumbers;
+        private object _lockObject;
+
+        public PrimeNumberRepository()
+        {
+            _primeNumbers = new Dictionary<long, int>();
+            _lockObject = new object();
+        }
+
+        public Task AddPrimeNumberAsync(long primeNumber)
+        {
+            lock (_lockObject)
+            {
+                if (_primeNumbers.ContainsKey(primeNumber))
+                    _primeNumbers[primeNumber]++;
+                else
+                {
+                    _primeNumbers.Add(primeNumber, 1);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task<List<long>> GetTopTenPrimeNumbersAsync()
+        {
+            var topTen = new List<long>();
+
+            lock (_lockObject)
+            {
+                var sortedPrimeNumbers = _primeNumbers.OrderByDescending(x => x.Value);
+
+                if (_primeNumbers.Count < 10)
+                    topTen = sortedPrimeNumbers.Select(x => x.Key).ToList();
+                else   
+                    topTen = sortedPrimeNumbers.Take(10).Select(x => x.Key).ToList();
+            }
+
+            return Task.FromResult(topTen);
+        }
+    }
+}
---
+++ PrimeNumberService.Common/Interfaces/IPrimeNumberRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberService.Common.Interfaces
+{
+    /// <summary>
+    /// Exposes methods to interact with the Prime Number data store for the application
+    /// </summary>
+    public interface IPrimeNumberRepository
+    {
+        Task AddPrimeNumberAsync(long primeNumber);
+        Task<List<long>> GetTopTenPrimeNumbersAsync();
+    }
+}
