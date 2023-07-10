bfc8247034ce6840f719fd73d28ec12b2736f900
austinthomas147 <austinthomas147@gmail.com>
2023-07-09 22:04:36 -0500

    Add interfaces for sender and response handler

---
+++ PrimeNumberService.Client/Interfaces/Handlers/IPrimeNumberResponseHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberService.Client.Interfaces.Handlers
+{
+    /// <summary>
+    /// Handles the responses for messages returned from the gRPC server
+    /// </summary>
+    public interface IPrimeNumberResponseHandler
+    {
+        Task HandlePrimeNumberResponsesAsync(List<PrimeNumberResponse> primeNumberResponses);
+    }
+}
---
+++ PrimeNumberService.Client/Interfaces/Senders/IPrimeNumberRequestSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberService.Client.Interfaces.Senders
+{
+    /// <summary>
+    /// The class responsible for sending the messages to the gRPC server and closing the stream
+    /// </summary>
+    public interface IPrimeNumberRequestSender
+    {
+        Task<List<PrimeNumberResponse>> SendPrimeNumberRequestsAsync();
+    }
+}
