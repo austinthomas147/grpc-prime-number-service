eb83d3a3f9fb5db4163a8029ab9821b1800b0cc1
austinthomas147 <austinthomas147@gmail.com>
2023-07-09 22:03:40 -0500

    Add response handler for doing things with the responses that come back from the server

---
+++ PrimeNumberService.Client/Implementations/Handlers/PrimeNumberResponseHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using PrimeNumberService.Client.Exceptions;
+using PrimeNumberService.Client.Interfaces.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberService.Client.Implementations.Handlers
+{
+    public class PrimeNumberResponseHandler : IPrimeNumberResponseHandler
+    {
+        private readonly ILogger<PrimeNumberResponseHandler> _logger;
+        private readonly int _expectedRequestCount;
+
+        public PrimeNumberResponseHandler(ILogger<PrimeNumberResponseHandler> logger,
+                                          int expectedResponseCount) 
+        {
+            _logger = logger;
+            _expectedRequestCount = expectedResponseCount;
+        }
+
+        public Task HandlePrimeNumberResponsesAsync(List<PrimeNumberResponse> primeNumberResponses)
+        {
+            if (primeNumberResponses.Count != _expectedRequestCount)
+                throw new MissingResponseException($"Expected {_expectedRequestCount} responses, got {primeNumberResponses.Count}");
+
+            primeNumberResponses.ForEach(response =>
+            {
+                var roundTripTimeInSeconds = DateTime.Now - new DateTime(response.IncomingTimestamp);
+
+                _logger.LogInformation($"Total round trip time for requestId: {response.RequestId} - {roundTripTimeInSeconds.TotalSeconds} seconds...");
+            });
+
+            return Task.CompletedTask;
+        }
+    }
+}
