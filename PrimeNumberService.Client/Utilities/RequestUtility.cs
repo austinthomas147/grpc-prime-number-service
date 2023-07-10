91a7e82b81fa989a7e7520410f643643b8bb1993
austinthomas147 <austinthomas147@gmail.com>
2023-07-09 22:05:40 -0500

    Add utility for retrieving request ids

---
+++ PrimeNumberService.Client/Utilities/RequestUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberService.Client.Utilities
+{
+    /// <summary>
+    /// Utility methods related to message requests
+    /// </summary>
+    public static class RequestUtility
+    {
+        private static int _requestId = 0;
+
+        /// <summary>
+        /// Returns the next incremented requestId to keep track of requests on the client side
+        /// </summary>
+        /// <returns></returns>
+        public static int GetNextRequestId()
+        {
+            return _requestId++;
+        }
+    }
+}
